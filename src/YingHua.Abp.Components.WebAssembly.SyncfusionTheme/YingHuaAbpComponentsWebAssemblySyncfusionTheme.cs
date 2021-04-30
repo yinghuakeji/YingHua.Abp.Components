3bb1222429448b59fe703e96cdb9bce2188d7064
tchivs <tchivs@yinghuatech.cn>
2021-04-30 14:58:44 +0800

    ad

--- src/YingHua.Abp.Components.WebAssembly.SyncfusionTheme/YingHuaAbpComponentsWebAssemblySyncfusionTheme.cs
+++ src/YingHua.Abp.Components.WebAssembly.SyncfusionTheme/YingHuaAbpComponentsWebAssemblySyncfusionTheme.cs
@@ -1,7 +1,6 @@
 ﻿using Volo.Abp.AspNetCore.Components.Web.BasicTheme;
 using Volo.Abp.AspNetCore.Components.Web.Theming.Routing;
 using Volo.Abp.AspNetCore.Components.Web.Theming.Toolbars;
-using Volo.Abp.AspNetCore.Components.WebAssembly.BasicTheme;
 using Volo.Abp.AspNetCore.Components.WebAssembly.Theming;
 using Volo.Abp.Http.Client.IdentityModel.WebAssembly;
 using Volo.Abp.Modularity;
