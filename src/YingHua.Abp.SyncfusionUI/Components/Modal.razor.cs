763caaef60e0b2e5d2c661e35220fcd73941aaa2
tchivs <topivn@163.com>
2021-05-17 18:27:27 +0800

    XX

---
+++ src/YingHua.Abp.SyncfusionUI/Components/Modal.razor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YingHua.Abp.SyncfusionUI.Components
+{
+    public partial class Modal
+    {
+        private bool IsVisible { get; set; } = true;
+        public void Hide()
+        {
+            this.IsVisible = false;
+        } public void Show()
+        {
+            this.IsVisible = true;
+        }
+    }
+}
