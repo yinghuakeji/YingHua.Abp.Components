f34631509dd382caa7145ae4ee4afad14f3c8a7a
tchivs <topivn@163.com>
2021-05-25 11:33:54 +0800

    add .net core theme

---
+++ src/YingHua.Abp.AspNetCore.Mvc.UI.Theme.Basic/Themes/Basic/Components/Brand/MainNavbarBrandViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace YingHua.Abp.AspNetCore.Mvc.UI.Theme.Basic.Themes.Basic.Components.Brand
+{
+    public class MainNavbarBrandViewComponent : AbpViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View("~/Themes/Basic/Components/Brand/Default.cshtml");
+        }
+    }
+}
---
+++ src/YingHua.Abp.AspNetCore.Mvc.UI.Theme.Basic/Themes/Basic/Components/MainNavbar/MainNavbarViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc;
+
+namespace YingHua.Abp.AspNetCore.Mvc.UI.Theme.Basic.Themes.Basic.Components.MainNavbar
+{
+    public class MainNavbarViewComponent : AbpViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View("~/Themes/Basic/Components/MainNavbar/Default.cshtml");
+        }
+    }
+}
